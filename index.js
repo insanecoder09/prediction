c09cc0b086a52e9c108339272d2488a0ca2a5907
insanecoder09 <147096842+insanecoder09@users.noreply.github.com>
2024-03-12 22:31:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,36 @@ import cors from 'cors';
 
 const app = express();
 
+const keys = [
+    "abc-xyz-mno-123",
+    "bhf-scs-rfr",
+    "dhr-chs-khy",
+    "dhe-wur-fht",
+    "wpt-vhf-elt"
+]
 
-
-const keys = ["abc-xyz-mno-123",]
+const keysData = {
+    "abc-xyz-mno-123": {
+        username: "Ayush Singh",
+        isUsed: true,
+    },
+    "bhf-scs-rfr":{
+        username:"Vivek"
+        isUsed:false,
+    }
+    "dhr-chs-khy":{
+        username:"Ram"
+        isUsed:false,
+    }
+    "dhe-wur-fht":{
+        username:"Prashant"
+        isUsed:true,
+    }
+    "wpt-vhf-elt":{
+        username:"Yash"
+        isUsed:true,
+    }
+}
 
 app.use(cors());
 
@@ -14,9 +41,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/prediction/:param', (req, res) => {
-    if(keys.includes(req.params.param))
-    res.send('1' + req.connection.remoteAddress)
-else res.send('0')
+    if (keys.includes(req.params.param)) {
+        if (keysData[req.params.param].isUsed) res.send('2');
+        else res.send('1');
+    }
+    else res.send('0')
 });
 
 app.listen(3000, () => console.log("Server Running"))
